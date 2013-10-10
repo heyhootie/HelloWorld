702e54db055b5a55c969351bed459e2a01c5f89e
heyhootie <heather.ryan@imulus.com>
2013-10-09 19:16:21 -0600

    initial HelloWorld

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+        public ActionResult Index()
+        {
+            //ViewData["message"] = "Hello, World";
+            ViewBag.Message = "Hello Imulus!";
+            
+            return View();
+        }
+
+    }
+}
